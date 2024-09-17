8d850dac21ce656d32e59b4ed445bab797464aca
AssilAB02 <150059237+AssilAB02@users.noreply.github.com>
2024-09-17 16:37:07 +0200

    Creación de la actividad CalendarActivity

--- app/src/main/java/com/example/aplicacindegestinderesiduos/MainActivity.kt
+++ app/src/main/java/com/example/aplicacindegestinderesiduos/MainActivity.kt
@@ -1,17 +1,6 @@
 package com.example.aplicacindegestinderesiduos
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.aplicacindegestinderesiduos.ui.theme.AplicaciónDeGestiónDeResiduosTheme
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
