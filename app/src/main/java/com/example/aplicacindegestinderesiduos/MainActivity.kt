c7a7b82d7c31dddcbe3c6649f4b13d00b01b2a6b
Barbosa <pbarboje@myuax.com>
2024-09-17 16:30:54 +0200

    Organización e implementación de mejoras en colors.xml

--- app/src/main/java/com/example/aplicacindegestinderesiduos/MainActivity.kt
+++ app/src/main/java/com/example/aplicacindegestinderesiduos/MainActivity.kt
@@ -1,22 +1,10 @@
 package com.example.aplicacindegestinderesiduos
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.aplicacindegestinderesiduos.ui.theme.AplicaciónDeGestiónDeResiduosTheme
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import com.example.aplicacindegestinderesiduos.R.*
-import com.example.aplicacindegestinderesiduos.ui.theme.StatisticsActivity
 
 class MainActivity : AppCompatActivity() {
 
