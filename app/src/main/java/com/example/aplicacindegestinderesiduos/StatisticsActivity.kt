9656668634da033cb964222d7bb67ee94ef5e53d
Barbosa <pbarboje@myuax.com>
2024-09-18 12:26:45 +0200

    Implementación de mejoras y logica del apartado de Estadísticas

--- app/src/main/java/com/example/aplicacindegestinderesiduos/StatisticsActivity.kt
+++ app/src/main/java/com/example/aplicacindegestinderesiduos/StatisticsActivity.kt
@@ -1,12 +1,27 @@
 package com.example.aplicacindegestinderesiduos
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class StatisticsActivity : AppCompatActivity(){
+class StatisticsActivity : AppCompatActivity() {
 
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_statistics)// Vincula el diseño de la actividad con el archivo XML
+        setContentView(R.layout.activity_statistics)
+
+        val pieChartView: PieChartView = findViewById(R.id.viewPieChart)
+        val tvRecycled: TextView = findViewById(R.id.tvRecycled)
+        val tvNonRecycled: TextView = findViewById(R.id.tvNonRecycled)
+
+        // Sample data
+        val recycled = 40f
+        val nonRecycled = 60f
+
+        pieChartView.setData(recycled, nonRecycled)
+        tvRecycled.text = "Residuos reciclados: $recycled kg"
+        tvNonRecycled.text = "Residuos no reciclados: $nonRecycled kg"
     }
 }
