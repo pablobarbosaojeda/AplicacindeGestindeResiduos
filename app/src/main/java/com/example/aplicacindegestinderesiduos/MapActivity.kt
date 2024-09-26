b34663dec169f3b10b6fb84e1de1dd464a643851
dsousa034 <150059798+dsousa034@users.noreply.github.com>
2024-09-26 17:23:26 +0200

    Mapa

--- app/src/main/java/com/example/aplicacindegestinderesiduos/MapActivity.kt
+++ app/src/main/java/com/example/aplicacindegestinderesiduos/MapActivity.kt
@@ -3,24 +3,13 @@ package com.example.aplicacindegestinderesiduos
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.aplicacindegestinderesiduos.ui.theme.AplicaciónDeGestiónDeResiduosTheme
 
 class MapActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_map)
 
-        val imageView: ImageView = findViewById(R.id.myImageView)
+        val imageView: ImageView = findViewById(R.id.imageView)
         imageView.setImageResource(R.drawable.puntolimpiovillanueva)
 
     }
